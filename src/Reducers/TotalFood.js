464b4f008b16ccbd8f5dbce36f9dfc1246cb26f5
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-06-14 13:28:12 +0100

    calcule prix de panier

---
+++ src/Reducers/TotalFood.js
@@ -0,0 +1,12 @@
+import { TOT_PRIX } from "../Action/types";
+
+const initialState = {
+  total: [],
+};
+
+const totalReducer = (state = initialState, action) => {
+  
+  return state;
+};
+
+export default totalReducer;
